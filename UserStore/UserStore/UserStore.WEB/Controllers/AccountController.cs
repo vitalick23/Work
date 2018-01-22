3a2ff213659a13d6d83cda057e2f6b7f5d335c1e
Vitaliy <vitali_fc_arsenal@mail.ru>
2018-01-22 20:09:46 +0300

    first

---
+++ UserStore/UserStore/UserStore.WEB/Controllers/AccountController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
+using UserStore.Models;
+using UserStore.BLL.DTO;
+using System.Security.Claims;
+using UserStore.BLL.Interfaces;
+using UserStore.BLL.Infrastructure;
+
+namespace UserStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUserService UserService
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<IUserService>();
+            }
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+          
+            if (ModelState.IsValid)
+            {
+                UserDTO userDto = new UserDTO { Email = model.Email, Password = model.Password};
+                ClaimsIdentity claim = await UserService.Authenticate(userDto);
+                if (claim == null)
+                {
+                    ModelState.AddModelError("", "Неверный логин или пароль.");
+                }
+                else
+                {
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, claim);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+                UserDTO userDto = new UserDTO
+                {
+                    Email = model.Email,
+                    Password = model.Password,
+                    Address = model.Address,
+                    Name = model.Name,
+                    Role = "user"
+                };
+                OperationDetails operationDetails = await UserService.Create(userDto);
+                if (operationDetails.Succedeed)
+                    return View("SuccessRegister");
+                else
+                    ModelState.AddModelError(operationDetails.Property, operationDetails.Message);
+            }
+            return View(model);
+        }
+        
+    }
+}
